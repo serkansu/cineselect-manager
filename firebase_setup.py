a4fbe4f0ca1f8cf26c1777a799153a076a675801
serkansu <suluns.tr@gmail.com>
2025-08-08 20:37:58 +0300

    Update  firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -4,6 +4,6 @@ from firebase_admin import credentials, firestore
 
 def get_firestore():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase-adminsdk.json")  # JSON dosyanın ismi buysa değişme!
+        cred = credentials.Certificate("/etc/secrets/firebase-adminsdk.json")  # Render için doğru yol
         firebase_admin.initialize_app(cred)
     return firestore.client()
